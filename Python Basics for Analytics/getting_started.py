18eef7a5b4608a8d64290a454e772885b75a0d13
Arpita Choudhury <achoudhury17@fordham.edu>
2023-03-29 16:08:23 +0530

    initial commit

---
+++ Python Basics for Analytics/getting_started.py
@@ -0,0 +1,26 @@
+x = 5
+y = 10
+print(x+y)
+
+from collections import Counter
+marriage_ages = [22, 22, 25, 25, 30, 24, 26, 24, 35]  # create a list
+value_counts = Counter(marriage_ages)  # apply the counter functionality
+print(value_counts.most_common())
+
+def add_two_numbers(x, y):  # function header
+    """
+    Takes in two numbers and returns the sum
+    parameters
+        x : str
+            first number
+        y : str
+            second number
+    returns
+        x+y
+    """
+    z = x + y
+    return z  # function return
+print(add_two_numbers(100,5))  # function  call
+
+y = lambda x, y: x + y  # an anonymous function which takes x and y and input and returns x+y
+print(y(100,5))  # call the function
