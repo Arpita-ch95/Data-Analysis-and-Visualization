457cae192e087156d0145346c2b4bcb45c4c5bbc
Arpita Choudhury <achoudhury17@fordham.edu>
2023-04-06 19:35:03 +0530

    data structures: lists & dictionaries

--- Python Basics for Analytics/data_structures.py
+++ Python Basics for Analytics/data_structures.py
@@ -54,3 +54,31 @@ print(depths)
 depths.insert(5, 1000)
 print("---10---")
 print(depths)
+
+my_set = set()
+my_set.add(1)
+my_set.add(2)
+my_set.add(1)
+# Note that the set only contains a single 1 value
+print("---0---")
+print(my_set)
+
+my_set2 = set()
+my_set2.add(1)
+my_set2.add(2)
+my_set2.add(3)
+my_set2.add(4)
+print("---1---")
+print(my_set2)
+
+# Prints the overlap
+print("---1---")
+print(my_set.intersection(my_set2))
+print("---2---")
+
+# Prints the combination
+print(my_set.union(my_set2))
+
+# Prints the difference (those in my_set but not my_set2)
+print("---3---")
+print(my_set.difference(my_set2))
